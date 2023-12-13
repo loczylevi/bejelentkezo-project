fd157384c7ffc07d0467980dde2586db39c8ceda
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-12-13 18:46:03 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,40 +12,67 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import streamlit as st
-from streamlit.logger import get_logger
+import sqlite3
 
-LOGGER = get_logger(__name__)
+import streamlit as szar
 
+#con = sqlite3.connect(':memory:')
+con = sqlite3.connect('titkos69.db')
+cur = con.cursor()
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS titkos_adatok
+    (nev        TEXT,
+    jelszo       TEXT,
+    e_mail       TEXT)
+    """)
 
 
-if __name__ == "__main__":
-    run()
+con.commit()
+
+opcio = szar.radio(
+    "Van már fiókod :thinking_face:?",
+    [":rainbow[Van]", "***Nincs***"],
+    index=None,
+)
+
+if opcio == ':rainbow[Van]':
+    szar.title('Bejelentkezés')
+    be_nev = szar.text_input('Név', placeholder="kattints ide...", type="default")
+    be_jel = szar.text_input('jelszó', placeholder="kattints ide...", type="password")
+    kereso = cur.execute(" SELECT * FROM titkos_adatok WHERE nev =? AND jelszo =?",(be_nev, be_jel))
+    lista1 = []
+    for sor in kereso:
+        lista1.append(sor)
+    if szar.button('Bejelentkezés',type="secondary"):
+        if len(lista1) > 0:
+            szar.title(f'{be_nev} sikeresen :blue[bejelentkezet] :sunglasses:!')
+            szar.balloons()
+
+        else:
+            szar.title(f'Ilyen felhasználó: "_{be_nev}_" :red[NEM] létezik!')
+
+elif opcio == '***Nincs***':
+    szar.title("Regisztráció")
+    reg_nev = szar.text_input('Név', placeholder="kattints ide...", type="default")
+    reg_jel = szar.text_input('jelszó', placeholder="kattints ide...", type="password")
+    reg_ema = szar.text_input('e-mail', placeholder="kattints ide...", type="default")
+    kereso2 = cur.execute(" SELECT * FROM titkos_adatok WHERE nev =? OR e_mail =?",(reg_nev, reg_ema))
+    lista2 = []
+    for sor in kereso2:
+        lista2.append(sor)
+
+    if szar.button('Regisztrálás',type="primary"):
+        if len(lista2) > 0:
+            szar.error(f'Figyelem ez a név: "_{lista2[0][0]}_" vagy ez az emali: "_{lista2[0][2]}_" már használatban van!', icon="🚨")
+        else:
+            cur.execute("INSERT INTO titkos_adatok VALUES (?,?,?) ", ( reg_nev, reg_jel, reg_ema))
+            con.commit()
+            szar.success(f'"_{reg_nev}_" felhasználó sikeresen regisztrálva!', icon="✅")
+
+
+            #for row in cur.execute("SELECT nev, jelszo, e_mail FROM titkos_adatok"):
+                #szar.write(row)
+
+
+
