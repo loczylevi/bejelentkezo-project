62a11ccf7cae452646eb776490d4573e08dbdbe8
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-12-15 12:22:45 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ import textwrap
 
 import streamlit as st
 
-
+"""
 def show_code(demo):
     """Showing the code of the demo."""
     show_code = st.sidebar.checkbox("Show code", True)
@@ -26,3 +26,4 @@ def show_code(demo):
         st.markdown("## Code")
         sourcelines, _ = inspect.getsourcelines(demo)
         st.code(textwrap.dedent("".join(sourcelines[1:])))
+"""
