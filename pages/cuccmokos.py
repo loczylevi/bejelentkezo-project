03c8e3c301928fee0cd908774af76ce4110d12a6
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-12-15 12:25:28 +0100

    Update cuccmokos.py

--- pages/cuccmokos.py
+++ pages/cuccmokos.py
@@ -42,34 +42,4 @@ if opcio == ':rainbow[Van]':
     be_jel = szar.text_input('jelszó', placeholder="kattints ide...", type="password")
     kereso = cur.execute(" SELECT * FROM titkos_adatok WHERE nev =? AND jelszo =?",(be_nev, be_jel))
     lista1 = []
-    for sor in kereso:
-        lista1.append(sor)
-    if szar.button('Bejelentkezés',type="secondary"):
-        if len(lista1) > 0:
-            szar.title(f'{be_nev} sikeresen :blue[bejelentkezet] :sunglasses:!')
-            szar.balloons()
 
-        else:
-            szar.title(f'Ilyen felhasználó: "_{be_nev}_" :red[NEM] létezik!')
-
-elif opcio == '***Nincs***':
-    szar.title("Regisztráció")
-    reg_nev = szar.text_input('Név', placeholder="kattints ide...", type="default")
-    reg_jel = szar.text_input('jelszó', placeholder="kattints ide...", type="password")
-    reg_ema = szar.text_input('e-mail', placeholder="kattints ide...", type="default")
-    kereso2 = cur.execute(" SELECT * FROM titkos_adatok WHERE nev =? OR e_mail =?",(reg_nev, reg_ema))
-    lista2 = []
-    for sor in kereso2:
-        lista2.append(sor)
-
-    if szar.button('Regisztrálás',type="primary"):
-        if len(lista2) > 0:
-            szar.error(f'Figyelem ez a név: "_{lista2[0][0]}_" vagy ez az emali: "_{lista2[0][2]}_" már használatban van!', icon="🚨")
-        else:
-            cur.execute("INSERT INTO titkos_adatok VALUES (?,?,?) ", ( reg_nev, reg_jel, reg_ema))
-            con.commit()
-            szar.success(f'"_{reg_nev}_" felhasználó sikeresen regisztrálva!', icon="✅")
-
-
-            #for row in cur.execute("SELECT nev, jelszo, e_mail FROM titkos_adatok"):
-                #szar.write(row)
